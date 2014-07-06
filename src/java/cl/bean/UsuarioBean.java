1e255efaf82b0625ec9e723f7583f98a50a7172d
zerox1819 <zerox1819@gmail.com>
2014-07-06 19:40:24 +0000

    primera version del proyecto en el repositorio

--- src/java/cl/bean/UsuarioBean.java
+++ src/java/cl/bean/UsuarioBean.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cl.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import cl.pojos.*;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -19,11 +16,79 @@ public class UsuarioBean {
 
     private String Rut;
     private String Clave;
-
     private Estudiante estudiante;    
-    private Tutor Tutor;
+    private Tutor tutor;
 
+    public String getRut() {
+        return Rut;
+    }
+
+    public void setRut(String Rut) {
+        this.Rut = Rut;
+    }
+
+    public String getClave() {
+        return Clave;
+    }
+
+    public void setClave(String Clave) {
+        this.Clave = Clave;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor Tutor) {
+        this.tutor = Tutor;
+    }
+
+    
+    
     public UsuarioBean() {
     }
 
+    public String Validad(){
+         
+        //crear sesionfactory y session
+        estudiante =(Estudiante) se.createQuery("from Estudiante e where e.rut='"+Rut+"'").uniqueResult();
+        tutor =(Tutor) se.createQuery("from Tutor e where e.rut='"+Rut+"'").uniqueResult();
+        if((estudiante) != null){
+           if(estudiante.getClave().equals(Clave)){
+               //crear una sesion 
+               //redireccionar al menu del estudiante
+       
+           }else{
+               //mostrar un mensaje que la clave es incorrecta
+            }
+        
+        }else if (tutor != null){
+               if(tutor.getClave().equals(Clave)){
+               //crear una sesion 
+               //redireccionar al menu del estudiante
+        
+           }else{
+               //mostrar un mensaje que la clave es incorrecta
+            }
+          
+        }else if (Rut.equals("admin") && Clave.equals("admin")){
+           //redireccionar al menu del admin
+         
+        }else{
+           //mostrar un mensaje de que el rut no existe
+        }
+       
+    }
+       public String cerrarSesion(){
+        estudiante = null;
+        return "index";
+    }
 }
